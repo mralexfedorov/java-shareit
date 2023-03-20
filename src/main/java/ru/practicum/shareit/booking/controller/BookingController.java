af449aff046766a0dafd69dbaa4c18878295096b
Aleksandr Fedorov <alexsfed@yandex.ru>
2023-03-21 01:36:25 +0400

    fix: чек замечаний.

--- src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.service.BookingService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
