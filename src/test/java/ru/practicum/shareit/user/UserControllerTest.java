b7cbe24b7398fcb3f2b503f9188514efb7cdf19b
Aleksandr Fedorov <alexsfed@yandex.ru>
2023-04-02 21:19:36 +0400

    fix: чек замечаний.

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -14,7 +14,6 @@ import ru.practicum.shareit.user.service.UserService;
 
 import java.nio.charset.StandardCharsets;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
