2102e3850920e8d155b49bb0a7b9492c13792bc2
Aleksandr Fedorov <alexsfed@yandex.ru>
2023-04-02 19:25:37 +0400

    fix: чек замечаний.

---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,102 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private MockMvc mvc;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserRepository userRepository;
+    private UserDto userDto1;
+    private UserDto userDto2;
+
+    @BeforeEach
+    void setUp() {
+        userDto1 = new UserDto(
+                1,
+                "John",
+                "john.doe@mail.com");
+        userDto2 = new UserDto(
+                2,
+                "Bob",
+                "bob.doe@mail.com");
+    }
+
+    @Test
+    void createUserAndCheck() throws Exception {
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto1))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto1.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto1.getName())))
+                .andExpect(jsonPath("$.email", is(userDto1.getEmail())));
+
+        mvc.perform(get("/users/" + userDto1.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto1.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto1.getName())))
+                .andExpect(jsonPath("$.email", is(userDto1.getEmail())));
+
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto2))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto2.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto2.getName())))
+                .andExpect(jsonPath("$.email", is(userDto2.getEmail())));
+
+        userDto2.setEmail("upd" + userDto2.getEmail());
+
+        mvc.perform(patch("/users/" + userDto2.getId())
+                        .content(mapper.writeValueAsString(userDto2))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto2.getId()), Integer.class))
+                .andExpect(jsonPath("$.name", is(userDto2.getName())))
+                .andExpect(jsonPath("$.email", is(userDto2.getEmail())));
+
+        mvc.perform(delete("/users/" + userDto1.getId())
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].id", is(userDto2.getId()), Integer.class))
+                .andExpect(jsonPath("$[0].name", is(userDto2.getName())))
+                .andExpect(jsonPath("$[0].email", is(userDto2.getEmail())));
+    }
+}
