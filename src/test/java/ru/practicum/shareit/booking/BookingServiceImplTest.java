62aef9cffdcc0668cbdf1d4b4297cd9ed022990f
Aleksandr Fedorov <alexsfed@yandex.ru>
2023-06-04 22:25:02 +0300

    fix: чек замечаний.

--- src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceImplTest.java
@@ -36,8 +36,8 @@ public class BookingServiceImplTest {
     @Test
     void saveBooking() {
         // given
-        UserDto userDto1 = new UserDto(0, "Vlad", "vlad@email.com");
-        UserDto userDto2 = new UserDto(0, "Bob", "bob.doe@mail.com");
+        UserDto userDto1 = new UserDto(0, "Vlad", "vlady@email.com");
+        UserDto userDto2 = new UserDto(0, "Bob", "joey.doe@mail.com");
 
         // when
         UserDto createdUser1 = userService.createUser(userDto1);
